f4e8a169cb564db07f846c1078a560eaaf4a03a0
Timo Wendt <timo@tjwendt.de>
2012-08-30 12:18:41 +0300

    Runtime toggle of navbar
    This commit is signifigantly modified for DU, however,
    we maintain original authorship for API. Navbar visibility
    has no hardware contraints. Visibility is determined entirely
    from user settings and sane defaults based on factory configuration
    and emulator build prop.
    
    Visibility state and bar view management is handled
    in NavigationController class in SystemUI
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/java/android/view/WindowManagerPolicy.java
    
    Change-Id: I6ac53b8c9f7fce6f9ca6b4ad7bf31a1c1e896863

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -10269,6 +10269,11 @@ public class WindowManagerService extends IWindowManager.Stub
     }
 
     @Override
+    public boolean needsNavigationBar() {
+        return mPolicy.needsNavigationBar();
+    }
+
+    @Override
     public boolean hasNavigationBar() {
         return mPolicy.hasNavigationBar();
     }
