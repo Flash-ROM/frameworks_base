6e1c5a38c11b36e916bfbeee6dc5132adc0f9406
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2016-11-23 05:26:54 -0500

    Battery tile: allow to disable custom battery style [1/2]
    Change-Id: Iddb6a99a158b6f70d74e30a71766e94eeae30adb

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7299,6 +7299,12 @@ public final class Settings {
         public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
 
         /**
+         * Whether to sync the battery tile stile with the statusbar battery icon style
+         * @hide
+         */
+        public static final String STATUS_BAR_BATTERY_STYLE_TILE = "status_bar_battery_style_tile";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
@@ -7380,7 +7386,8 @@ public final class Settings {
             SYSTEM_NAVIGATION_KEYS_ENABLED,
             QS_TILES,
             STATUS_BAR_SHOW_BATTERY_PERCENT,
-            STATUS_BAR_BATTERY_STYLE
+            STATUS_BAR_BATTERY_STYLE,
+            STATUS_BAR_BATTERY_STYLE_TILE
         };
 
         /**
