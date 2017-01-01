1fe345af2c10e4cad7518bbba00e67e367b80ab8
ezio84 <brabus84@gmail.com>
2016-12-31 22:50:55 -0500

    Night Light brightness mode options [1/2]
    Squash of:
    *Night tile: automatically set a lower brightness when active [1/2]
    *Night mode tile brightness: allow to set a different value [1/2]
    *Night light tile brightness: merge the two options into one [1/2]
    *Night light auto low brightness: wider action [1/2]
    *Fix manual brightness going to 0 after reboot
    
    @nathanchance edit: Changed customVal values to make more sense (0 is disabled and 3 is medium); also cleaned up alignment

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7347,6 +7347,12 @@ public final class Settings {
         public static final String LOCKSCREEN_TARGETS = "lockscreen_target_actions";
 
         /**
+         * Low brightness level for the advanced night mode tile
+         * @hide
+         */
+        public static final String QS_NIGHT_BRIGHTNESS_VALUE = "qs_night_brightness_value";
+
+        /**
          * Status bar battery %
          * 0: Hide the battery percentage
          * 1: Display the battery percentage inside the icon
