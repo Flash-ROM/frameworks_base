e9ebecf145c5c956d461dd8ffb951bf3be29e694
Josh Chasky <jchasky@gmail.com>
2017-01-16 11:06:49 -0500

    Fix OmniSwitch non-protected broadcast logspam
    E/ActivityManager(548): Sending non-protected broadcast org.omnirom.omniswitch.ACTION_HIDE_OVERLAY from system 3232:com.android.systemui/u0a27 pkg com.android.systemui
    E/ActivityManager(548): java.lang.Throwable
    E/ActivityManager(548):  at com.android.server.am.ActivityManagerService.checkBroadcastFromSystem(ActivityManagerService.java:17862)
    E/ActivityManager(548):  at com.android.server.am.ActivityManagerService.broadcastIntentLocked(ActivityManagerService.java:18343)
    E/ActivityManager(548):  at com.android.server.am.ActivityManagerService.broadcastIntent(ActivityManagerService.java:18525)
    E/ActivityManager(548):  at android.app.ActivityManagerNative.onTransact(ActivityManagerNative.java:500)
    E/ActivityManager(548):  at com.android.server.am.ActivityManagerService.onTransact(ActivityManagerService.java:2796)
    E/ActivityManager(548):  at android.os.Binder.execTransact(Binder.java:565)
    
    Change-Id: I95ee5ccccad486a10b12a9381a2c9d2e65417209

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -4486,6 +4486,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                         || actionName.startsWith("com.android.server.sip.SipWakeupTimer")
                         || actionName.startsWith("com.android.internal.telephony.data-reconnect")
                         || actionName.startsWith("android.net.netmon.launchCaptivePortalApp")
+                        || actionName.startsWith("org.omnirom.omniswitch")
                         || actionName.startsWith("intent_navbar_edit")) {
                     return true;
                 }
