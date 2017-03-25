DirtyUnicorns/android_frameworks_base

Recents: Don't show alternate recents when task is locked
Simulate AOSP behavior and don't show any recent task managers
when screenpinning is on. This solution works and is simple.
However, a proper solution would involve handling all this
in the activity stack management, as is the case with AOSP recents.

Change-Id: I957659d6ef251e532b55eb7dfca8e0f06ff51b49

packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -267,6 +267,8 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     protected boolean mVrMode;
 
+    protected boolean mScreenPinningEnabled;
+
     @Override  // NotificationData.Environment
     public boolean isDeviceProvisioned() {
         return mDeviceProvisioned;
@@ -1308,7 +1310,11 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     @Override
     public void screenPinningStateChanged(boolean enabled) {
-        Log.d(TAG, "StatusBar API screenPinningStateChanged = " + enabled);
+        if (mScreenPinningEnabled != enabled) {
+            mScreenPinningEnabled = enabled;
+        }
+        if (DEBUG)
+            Log.d(TAG, "StatusBar API screenPinningStateChanged = " + enabled);
     }
 
     protected H createHandler() {
@@ -1367,8 +1373,10 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     protected void showRecents(boolean triggeredFromAltTab, boolean fromHome) {
         if (isOmniSwitchEnabled()) {
-            Intent showIntent = new Intent(DuUtils.ACTION_SHOW_OVERLAY);
-            mContext.sendBroadcastAsUser(showIntent, UserHandle.CURRENT);
+            if (!mScreenPinningEnabled) {
+                Intent showIntent = new Intent(DuUtils.ACTION_SHOW_OVERLAY);
+                mContext.sendBroadcastAsUser(showIntent, UserHandle.CURRENT);
+            }
         } else {
             if (mRecents != null) {
                 sendCloseSystemWindows(SYSTEM_DIALOG_REASON_RECENT_APPS);
@@ -1379,8 +1387,10 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     protected void hideRecents(boolean triggeredFromAltTab, boolean triggeredFromHomeKey) {
         if (isOmniSwitchEnabled()) {
-            Intent showIntent = new Intent(DuUtils.ACTION_HIDE_OVERLAY);
-            mContext.sendBroadcastAsUser(showIntent, UserHandle.CURRENT);
+            if (!mScreenPinningEnabled) {
+                Intent showIntent = new Intent(DuUtils.ACTION_HIDE_OVERLAY);
+                mContext.sendBroadcastAsUser(showIntent, UserHandle.CURRENT);
+            }
         } else if (mRecents != null) {
             mRecents.hideRecents(triggeredFromAltTab, triggeredFromHomeKey);
         }
@@ -1388,8 +1398,10 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     protected void toggleRecents() {
         if (isOmniSwitchEnabled()) {
-            Intent showIntent = new Intent(DuUtils.ACTION_TOGGLE_OVERLAY);
-            mContext.sendBroadcastAsUser(showIntent, UserHandle.CURRENT);
+            if (!mScreenPinningEnabled) {
+                Intent showIntent = new Intent(DuUtils.ACTION_TOGGLE_OVERLAY);
+                mContext.sendBroadcastAsUser(showIntent, UserHandle.CURRENT);
+            }
         } else if (mRecents != null) {
             mRecents.toggleRecents(mDisplay);
         }

Flash-ROM/frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -272,6 +272,8 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     private Set<String> mNonBlockablePkgs;
 
+    protected boolean mScreenPinningEnabled;
+
     @Override  // NotificationData.Environment
     public boolean isDeviceProvisioned() {
         return mDeviceProvisioned;
@@ -1323,7 +1325,11 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     @Override
     public void screenPinningStateChanged(boolean enabled) {
-        Log.d(TAG, "StatusBar API screenPinningStateChanged = " + enabled);
+        if (mScreenPinningEnabled != enabled) {
+            mScreenPinningEnabled = enabled;
+        }
+        if (DEBUG)
+            Log.d(TAG, "StatusBar API screenPinningStateChanged = " + enabled);
     }
 
     protected H createHandler() {
@@ -1382,8 +1388,10 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     protected void showRecents(boolean triggeredFromAltTab, boolean fromHome) {
         if (isOmniSwitchEnabled()) {
-            Intent showIntent = new Intent(FlashUtils.ACTION_SHOW_OVERLAY);
-            mContext.sendBroadcastAsUser(showIntent, UserHandle.CURRENT);
+            if (!mScreenPinningEnabled) {
+                Intent showIntent = new Intent(FlashUtils.ACTION_SHOW_OVERLAY);
+                mContext.sendBroadcastAsUser(showIntent, UserHandle.CURRENT);
+            }
         } else {
             if (mRecents != null) {
                 sendCloseSystemWindows(SYSTEM_DIALOG_REASON_RECENT_APPS);
@@ -1394,8 +1402,10 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     protected void hideRecents(boolean triggeredFromAltTab, boolean triggeredFromHomeKey) {
         if (isOmniSwitchEnabled()) {
-            Intent showIntent = new Intent(FlashUtils.ACTION_HIDE_OVERLAY);
-            mContext.sendBroadcastAsUser(showIntent, UserHandle.CURRENT);
+            if (!mScreenPinningEnabled) {
+                Intent showIntent = new Intent(FlashUtils.ACTION_HIDE_OVERLAY);
+                mContext.sendBroadcastAsUser(showIntent, UserHandle.CURRENT);
+            }
         } else if (mRecents != null) {
             mRecents.hideRecents(triggeredFromAltTab, triggeredFromHomeKey);
         }
@@ -1403,8 +1413,10 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     protected void toggleRecents() {
         if (isOmniSwitchEnabled()) {
-            Intent showIntent = new Intent(FlashUtils.ACTION_TOGGLE_OVERLAY);
-            mContext.sendBroadcastAsUser(showIntent, UserHandle.CURRENT);
+            if (!mScreenPinningEnabled) {
+                Intent showIntent = new Intent(FlashUtils.ACTION_TOGGLE_OVERLAY);
+                mContext.sendBroadcastAsUser(showIntent, UserHandle.CURRENT);
+            }
         } else if (mRecents != null) {
             mRecents.toggleRecents(mDisplay);
         }